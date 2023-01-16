467a47c62e46510a69a7a43ddac82f6039ff6d57
unknown <bayuxbca@gmail.com>
2023-01-16 13:46:22 +0700

    update APi controller & routing APi

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -16,6 +17,31 @@ class PostController extends Controller
 
         // return collection of posts as a resource
         return new PostResource(true, 'List Data Posts', $posts);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:png,jpg,jpeg,gif,svg|max:2048',
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan', $post);
     }
 
 }
